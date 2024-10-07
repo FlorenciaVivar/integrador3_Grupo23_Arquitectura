dcc16e92a2cdf30e76c2bf58f722f22525efa36e
Florencia Vivar <daianavivar93@gmail.com>
2024-10-07 19:26:40 -0300

    se crea proyecto

---
+++ src/main/java/tp3/integrador3_grupo23_arqui/Integrador3Grupo23ArquiApplication.java
@@ -0,0 +1,13 @@
+package tp3.integrador3_grupo23_arqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Integrador3Grupo23ArquiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Integrador3Grupo23ArquiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tp3/integrador3_grupo23_arqui/Integrador3Grupo23ArquiApplicationTests.java
@@ -0,0 +1,13 @@
+package tp3.integrador3_grupo23_arqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Integrador3Grupo23ArquiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
