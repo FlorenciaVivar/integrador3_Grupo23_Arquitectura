adc3bc69a0fa90980435dff7773bb1fdf27f1547
santiago cusato <santiagocusato0@gmail.com>
2024-10-09 20:18:18 -0300

    creacion de service, consultar con los muchachos lo comentado

---
+++ src/main/java/tp3/integrador3_grupo23_arqui/service/CarreraService.java
@@ -0,0 +1,46 @@
+package tp3.integrador3_grupo23_arqui.service;
+
+
+import aj.org.objectweb.asm.commons.TryCatchBlockSorter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tp3.integrador3_grupo23_arqui.model.Carrera;
+import tp3.integrador3_grupo23_arqui.repository.CarreraRepository;
+
+import java.util.List;
+
+
+@Service("CarreraService")
+
+public class CarreraService {
+
+    @Autowired
+    private  CarreraRepository carreraRepository;
+
+
+
+            public Iterable<Carrera> ObtenerCarreras() throws Exception {
+                return carreraRepository.findAll();
+            }
+            public Integer CarreraPorId(Integer id) throws Exception {
+                try {
+                  Carrera carreraAux= carreraRepository.getReferenceById(id);
+                  return carreraAux.getIdCarrera();
+                  }
+                catch (Exception e) {
+                    throw new Exception(e.getMessage());
+                }
+
+
+            }
+            public Iterable<Carrera> ObtenerCarrerasConEstudiantes() throws Exception{
+                try {
+                    Iterable<Carrera> AuxCarrera = carreraRepository.findCarrerasByEstudiantesOrdenada();
+                    return AuxCarrera;
+                }
+                catch (Exception e) {
+                    throw new Exception(e.getMessage());
+                }
+
+            }
+}
